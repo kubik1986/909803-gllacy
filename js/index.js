44768c81af40313982aae0a028d191f0171aa6c3
Viacheslav Akulov <sentence@ngs.ru>
2018-12-15 17:38:40 +0700

    Added scipts for slider and feedback form.

--- js/index.js
+++ js/index.js
@@ -21,3 +21,125 @@ function init(){
 
   myMap.geoObjects.add(myPlacemark);
 };
+
+// Slider
+var sliderToggles = document.querySelectorAll('.slider__toggle');
+var slides = document.querySelectorAll('.slider__item');
+var currentToggle = document.querySelector('.slider__toggle--current');
+var currentSlide = document.querySelector('.slider__item--current');
+
+var addToggledHandler = function(toggle, slide) {
+  toggle.addEventListener('click', function() {
+    if (!toggle.classList.contains('slider__toggle--current')) {
+      currentToggle.classList.remove('slider__toggle--current');
+      currentToggle.removeAttribute('aria-label');
+      toggle.classList.add('slider__toggle--current');
+      for (var i = 0; i < sliderToggles.length; i++) {
+        if (toggle === sliderToggles[i]) {
+          toggle.setAttribute('aria-label', 'Текущий слайд ' + (i+1));
+        }
+      }
+      currentToggle = toggle;
+      currentSlide.classList.remove('slider__item--current');
+      slide.classList.add('slider__item--current');
+      currentSlide = slide;
+    }
+  });
+};
+
+for (var i = 0; i < sliderToggles.length; i++) {
+  addToggledHandler(sliderToggles[i], slides[i]);
+}
+
+// Feedback pop-up
+var modalOverlay = document.querySelector('.modal-overlay');
+var modalPopup = document.querySelector('.modal-feedback');
+var modalForm = modalPopup.querySelector('.feedback-form');
+var modalOpenBtn = document.querySelector('.contacts__button');
+var modalCloseBtn = modalPopup.querySelector('.modal-feedback__close-button');
+var modalInputName = modalForm.querySelector('input[type="text"]');
+var modalInputEmail = modalForm.querySelector('input[type="email"]');
+var modalInputMsg = modalForm.querySelector('textarea');
+var fields = modalForm.querySelectorAll('.input');
+
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+try {
+  storageName = localStorage.getItem('name');
+  storageEmail = localStorage.getItem('email');
+}
+catch (err) {
+  isStorageSupport = false;
+}
+
+var clearStatus = function() {
+  for (var i = 0; i < fields.length; i++) {
+    if (fields[i].classList.contains('input--error')) {
+      fields[i].classList.remove('input--error');
+    }
+    if (fields[i].hasAttribute('aria-invalid')) {
+      fields[i].removeAttribute('aria-invalid');
+    }
+  }
+}
+
+modalOpenBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  clearStatus();
+  modalOverlay.classList.add('modal-overlay--active');
+  if (storageName && storageEmail) {
+    modalInputName.value = storageName;
+    modalInputEmail.value = storageEmail;
+    modalInputMsg.focus();
+  }
+  else {
+    modalInputName.focus();
+  }
+  window.addEventListener("keydown", function (evtEsc) {
+    if (evtEsc.keyCode === 27) {
+      evtEsc.preventDefault();
+      modalOverlay.classList.remove('modal-overlay--active');
+      modalOpenBtn.focus();
+    }
+  });
+});
+
+modalCloseBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalOverlay.classList.remove('modal-overlay--active');
+  modalOpenBtn.focus();
+});
+
+modalCloseBtn.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 9 && !evt.shiftKey) {
+    evt.preventDefault();
+    modalInputName.focus();
+  }
+});
+
+modalInputName.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 9 && evt.shiftKey) {
+    evt.preventDefault();
+    modalCloseBtn.focus();
+  }
+});
+
+modalForm.addEventListener('submit', function(evt) {
+  clearStatus();
+  for (var i = 0; i < fields.length; i++) {
+    if (!fields[i].value) {
+      evt.preventDefault();
+      fields[i].offsetWidth = fields[i].offsetWidth;
+      fields[i].classList.add('input--error');
+      fields[i].setAttribute('aria-invalid', 'true');
+      fields[i].focus();
+      return;
+    }
+  }
+  if(isStorageSupport) {
+    localStorage.setItem('name', modalInputName.value);
+    localStorage.setItem('email', modalInputEmail.value);
+  }
+});
